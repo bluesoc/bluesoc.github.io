02e899d41da13d3deb87df325fa60f8609cad86f
bluesoc <145991084+bluesoc@users.noreply.github.com>
2024-08-27 19:39:41 -0300

    Optimize code & improve UI effects

--- js/app.js
+++ js/app.js
@@ -8,10 +8,6 @@ function effectX(obj, translatePX, delay) {
         opacity: [0, 1]
       });
 }
-
-effectX("#app-left", 50, 1600);
-effectX("#app-right", -50, 1600);
-
 function FadeIn(obj, delay) {
     anime({
         targets: obj,
@@ -20,19 +16,25 @@ function FadeIn(obj, delay) {
         easing: 'easeOutExpo',
     })
 }
+function SlideUp(obj, delay) {
+    anime({
+        targets: obj,
+        translateY: [80, 0],
+        duration: delay,
+        opacity: [0, 1],
+        ease: "easeInQuad"
+    });
+}
+effectX("#app-left", 50, 1600);
+effectX("#app-right", -50, 1600);
 
 // Fade Main Title
-let headtitle = document.getElementsByClassName("main");
-
-FadeIn(headtitle, 2000)
+let headtitle = document.getElementById("head-intro");
 
 // Slide Up Effect
-let imgflex = document.getElementsByClassName("imgflex");
+let imghead = document.getElementById("head-img");
 
-anime({
-    targets: imgflex,
-    translateY: [80, 0],
-    duration: 4000,
-    opacity: [0, 1],
-    ease: "easeInQuad"
-});
+FadeIn(headtitle, 2000)
+SlideUp(imghead, 4000);
+
+SlideUp(".stack", 2000);
