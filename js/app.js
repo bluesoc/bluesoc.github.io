8d85e50081b2392c37b4764af9293f3cb89d975b
bluesoc <145991084+bluesoc@users.noreply.github.com>
2024-09-08 10:29:08 -0300

    perf: Refactor JS script 🧰

--- js/app.js
+++ js/app.js
@@ -1,56 +1,72 @@
 // Setup Animation
-function effectX(obj, translatePX, delay) {
+function effectX(obj, translatePX, duration) {
     anime({
         targets: obj,
         translateX: [-translatePX, 0],
         easing: 'easeInQuad',
-        duration: delay,
+        duration: duration,
         opacity: [0, 1]
       });
 }
 
-function FadeIn(obj, delay) {
+function FadeIn(obj, duration) {
     anime({
         targets: obj,
         opacity: [0, 1],
-        duration: delay,
+        duration: duration,
         easing: 'linear',
     })
 }
 
-function SlideUp(obj, delay) {
+function SlideUp(obj, duration) {
     anime({
         targets: obj,
         translateY: [80, 0],
-        duration: delay,
+        duration: duration,
         opacity: [0, 1],
         ease: "easeInQuad"
     });
 }
 
-effectX("#app-left", 50, 1600);
-effectX("#app-right", -50, 1600);
-
 // Fade Main Title
-let headtitle = document.getElementById("head-intro");
+FadeIn("#head-intro", 2000);
 
 // Slide Up Effect
-let imghead = document.getElementById("head-img");
+SlideUp("#head-img", 4000);
 
-FadeIn(headtitle, 2000)
-SlideUp(imghead, 4000);
+effectX("#app-left", 50, 1600);
+effectX("#app-right", -50, 1600);
 
 SlideUp(".stack", 2000);
 
 // Fade carousel
-FadeIn('.carousel', 3000)
+FadeIn('.carousel', 3000);
 
-// loops: how many times the carousel will be duplicated
-let loops = 3;
+// Carousel Handling
+(function() {
+    // loops: how many times the carousel will be duplicated
+    const CAROUSEL_LOOPS = 5;
 
-// Append elements
-while (loops > 0) {
-    let clone = document.querySelector('.carousel-inner').cloneNode(true);
-    document.querySelector(".carousel").appendChild(clone);
-    loops--;
-}
+    const carouselTop = document.querySelector(".carousel");
+    const carouselBottom = document.querySelector(".carousel-2")
+
+    const carouselTopInner = document.querySelector('.carousel-inner');
+    const carouselBottomInner = document.querySelector('.carousel-inner-2');
+
+    // Duplicate top carousel
+    for (let i = 0; i < CAROUSEL_LOOPS; i++) {
+        const clone = carouselTopInner.cloneNode(true);
+        carouselTop.appendChild(clone);
+    }
+
+    // Append bottom carousel with top carousel
+    while (carouselTopInner.lastChild) {
+        carouselBottomInner.appendChild(carouselTopInner.lastChild);
+    }
+
+    // Duplicate bottom carousel
+    for (let i = 0; i < 3; i++) {
+        const reclone = carouselBottomInner.cloneNode(true);
+        carouselBottom.appendChild(reclone);
+    }
+}) ();
